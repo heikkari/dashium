22d1b311b3610270a592eaa3fb6b942b91ab74b2
heikkari <bloody.ruby@tutanota.com>
2021-09-03 18:22:10 +0300

    Add a route & a test for /updateGJAccSettings20.php

--- lib/routes/user.ex
+++ lib/routes/user.ex
@@ -39,6 +39,53 @@ defmodule Routes.User do
     end
   end
 
+  post "/updateGJAccSettings20.php" do
+    if Utils.is_field_missing [ "accountID" ], conn.params do
+      send(conn, 400, "-1")
+    else
+      id = conn.params["accountID"] |> String.to_integer
+      mapped_fields = %{
+        "message_state" => [ key: "mS", min: 0, max: 2 ],
+        "friends_state" => [ key: "frS", min: 0, max: 1 ],
+        "comment_history_state" => [ key: "cS", min: 0, max: 2 ],
+        "youtube" => [ key: "yt", min: nil, max: nil ],
+        "twitter" => [ key: "twitter", min: nil, max: nil ],
+        "twitch" => [ key: "twitch", min: nil, max: nil ]
+      }
+
+      user = User.get(id)
+        |> Map.from_struct()
+        |> Enum.filter(fn { user_key, _ } -> mapped_fields[Atom.to_string user_key] !== nil end)
+        |> Enum.map(fn { user_key, _ } ->
+          [ key: key, min: min, max: max ] = mapped_fields[Atom.to_string user_key]
+          val = conn.params[key]
+
+          if val !== nil do
+            if min === nil do
+              { "$set", %{ user_key => val } }
+            else
+              if val > min or val < max do
+                { "$set", %{ user_key => val } }
+              end
+            end
+          end
+        end)
+        |> Enum.filter(&(&1 !== nil))
+        |> Enum.into(%{})
+
+      result = case Mongo.update_one(:mongo, "users", %{ _id: id }, user) do
+        { :error, _ } -> false
+        { :ok, _ } -> true
+      end
+
+      send(
+        conn,
+        (if result, do: 200, else: 500),
+        (if result, do: "1", else: "-1")
+      )
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "Not found!")
   end
--- test/api/user_test.exs
+++ test/api/user_test.exs
@@ -3,10 +3,9 @@ defmodule Api.UserTest do
   use Plug.Test
 
   @options Router.init([])
-  @register "/database/accounts/registerGJAccount.php"
-  @login "/database/accounts/loginGJAccount.php"
   @user_search "/database/getGJUsers20.php"
   @user_get "/database/getGJUserInfo20.php"
+  @user_update "/database/updateGJAccSettings20.php"
   @content_type "application/x-www-form-urlencoded"
 
   test "Testing /getGJUserInfo20.php" do
@@ -32,6 +31,23 @@ defmodule Api.UserTest do
 
     # Search for user
     reply = conn(:post, @user_search, %{ str: params.userName })
+      |> put_req_header("content-type", @content_type)
+      |> Router.call(@options)
+
+    assert reply.state   == :sent
+    assert reply.status  == 200
+  end
+
+  test "Testing /updateGJAccSettings20.php" do
+    # Register an account
+    params = Utils.test_register() |> Enum.at(0)
+
+    # Then get its ID
+    reply = Utils.test_login(params)
+    id = reply.resp_body |> String.split(",") |> Enum.at(0)
+
+    # Update the user
+    reply = conn(:post, @user_update, %{ accountID: id, mS: 1 })
       |> put_req_header("content-type", @content_type)
       |> Router.call(@options)
 
